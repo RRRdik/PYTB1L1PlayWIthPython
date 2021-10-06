13ad25ec1271fa6a6bd2f325887e9bcb0af36bb3
RRRdik <90895036+RRRdik@users.noreply.github.com>
2021-10-06 13:07:11 +0200

    Poem commit 1

---
+++ poem.py
@@ -0,0 +1,12 @@
+vers1 = "Ik heb twee katten en een hond"
+vers2 = "Lasagna vind ik heerlijk in mijn mond"
+vers3 = "Later wil ik game developer worden"
+vers4 = "Maar dan werk ik wel in een dagorde"
+vers5 = "Ik wil soms veel dingen kopen"
+vers6 = "Maar met geld kom ik dan in de knopen"
+vers7 = "Mijn naam is Ronel"
+vers8 = "Maar ondertussen weet je dat nu wel"
+vers9 = "Ik woon in Enkhuizen"
+vers10 = "Maar ik hoop hier nog niet weg te veruizen"
+
+print(vers1,vers2,vers3,vers4,vers5,vers6,vers7,vers8,vers9,vers10)
