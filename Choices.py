597104c0c4ddb7c1cbf1fdfe3bae89fa97f5b1e9
RRRdik <90895036+RRRdik@users.noreply.github.com>
2021-10-15 10:58:16 +0200

    Choices

---
+++ Choices.py
@@ -0,0 +1,31 @@
+print("What do you first do when you get home from school?")
+if input("A chores, B play games: ") == "A":
+    print("You like to get the important stuff out first and then do the fun stuff don't you?")
+else:
+    print("You do the fun stuff first so you can put your mind on the important stuff better afterwards.")
+
+print("Do you rather play with friends LAN or online?")
+if input("A LAN, B Online: ") == "A":
+    print("You like playing against eachother on splitscreen and cheat to see their location in-game.")
+else:
+    print("You like to have the screen all to yourself to get the most real gaming experience with your friends.")
+
+print("Would you rather play singleplayer or multiplayer?")
+if input("A singleplayer, B multiplayer: ") == "A":
+    print("You like to have everything be as immersive as it can while you take your time to enjoy it all for yourself.")
+else:
+    print("You like to have your friends around in-game for a more cozy feeling or just to mess around.")
+
+print("Do you pay for cosmetics or are you free to play?")
+if input("A Pay, B f2p: ") == "A":
+    print("You like looking cool and don't mind spending a few bucks to do so.")
+else:
+    print("You enjoy the game for the gameplay and not your personal looks.")
+
+print("Do you play competitevely or just for fun?: ")
+if input("A competitive, B fun: ") == "A":
+    print("You want to aim for the top and try your best to achieve it.")
+else:
+    print("You play for the fun and don't mind people being better than you.")
+
+    
